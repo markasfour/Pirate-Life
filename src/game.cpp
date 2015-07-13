d73b0a89cf55a7246e4979046b275f26f2ad2f31
Mark Asfour <mj4sparrow@gmail.com>
2015-07-12 21:02:23 -0700

    added sailing functionality

--- src/game.cpp
+++ src/game.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+#include <time.h>
 using namespace std;
 
+//global variables
 int HEALTH = 100;
 int WEALTH = 0;
 string SHIP = "Sloop";
@@ -13,16 +15,18 @@ vector <string> CREW;
 //prototypes
 void game_play();
 
+//clear screen
 void clear() 
 {
 	// CSI[2J clears screen, CSI[H moves the cursor to top-left corner
     cout << "\x1B[2J\x1B[H";
 }
 
+//used first time playing game
 bool init()
 {
 	cout << "Ahoy thar! Welcome to the Pirate Game! ARRGH!" << endl;
-	cout << "Would ye like to play? (Y/N)";
+	cout << "Would ye like to play? (Y/N) ";
 	string input = "";
 	while (input != "Y" && input != "N")
 	{
@@ -35,13 +39,14 @@ bool init()
 	else if(input == "Y")
 	{
 		clear();
-		cout << "That be the spirit! What be yer name?"; 
+		cout << "That be the spirit! What be yer name? "; 
 		cin >> NAME;
 		return true;
 	}
 	
 }
 
+//main menu
 void land_menu()
 {
 	cout << "                      MENU                " << endl;
@@ -58,6 +63,17 @@ void land_menu()
 
 }
 
+//sea menu
+void sea_menu()
+{
+	cout << "                    Navigate" << endl;
+	cout << "    --------------------------------------" << endl;
+	cout << "    |   1.    Continue sailing           |" << endl;
+	cout << "    |   2.    Return to Land             |" << endl;
+	cout << "    --------------------------------------" << endl;
+}
+
+//used first time playing game
 void tutorial_init()
 {
 	clear();
@@ -80,8 +96,116 @@ void tutorial_init()
 
 void sail() //1
 {
-	clear();
-	game_play();
+	int next = 0;
+	int injury = 0;
+	int treasure = 0;
+
+	sea_menu();
+	cout << "What woul\' ye like to do, capt\'n " << NAME << "? " << endl;
+	string input = "";
+	while (input != "1" && input != "2")
+	{
+		cin >> input;
+	}
+	if (input == "1")
+	{
+		next = rand() % 100;
+		if (next < 20)
+		{
+			cout << "It be smooth sailin\'." << endl;
+			sail();
+		}
+		else if (next < 30)
+		{
+			cout << "Ye got battered up by some stormy weather! ARRGH!" << endl;
+			injury = rand() % 3 + 1; //from 1 to 3
+			cout << "Health -" << injury;
+			HEALTH -= injury;
+			sail();
+		}
+		else if (next < 45)
+		{
+			if (CREW.size() > 0)
+			{
+				if (next % 3 == 0)
+				{
+					cout << "Yer crew came down wit\' scurvy! ARRGH!" << endl;
+					injury = rand() % 5 + 5; //from 5 to 10
+					cout << "Health -" << injury;
+					HEALTH -= injury;
+				}
+				else if (next % 3 == 1)
+				{
+					cout << "Yer crew started a fight on board yer ship! Them scalleywags!" << endl;
+					injury = rand() % 2 + 5; //from 2 to 7
+					cout << "Health -" << injury;
+					HEALTH -= injury;
+				}
+				else
+				{
+					cout << "Yer crew didn\'t get any shut eye!" << endl;
+					injury = 1;
+					cout << "Health -" << injury;
+					HEALTH -= injury;
+				}
+			}
+			else
+			{
+				if (next % 3 == 0)
+				{
+					cout << "Ye came down wit\' scurvy! ARRGH!" << endl;
+					injury = rand() % 5 + 5; //from 5 to 10
+					cout << "Health -" << injury;
+					HEALTH -= injury;
+				}
+				else if (next % 3 == 1)
+				{
+					cout << "Ye got sea sick from some rough tides!" << endl;
+					injury = rand() % 2 + 5; //from 2 to 7
+					cout << "Health -" << injury;
+					HEALTH -= injury;
+				}
+				else
+				{
+					cout << "Yer couldn\'t get any shut eye!" << endl;
+					injury = 1;
+					cout << "Health -" << injury;
+					HEALTH -= injury;
+				}
+			}
+			sail();
+		}
+		else if (next < 60)
+		{
+			cout << "Ye came across some floatin\' treasure!" << endl;
+			if (next % 2 == 0)
+			{
+				treasure = rand() % 10 + 1 + 5; //from 5 to 15
+			}
+			else 
+			{
+				treasure = rand() % 5 + 1; //from 1 to 5
+			}
+			cout << "Wealth +" << treasure << endl;
+			WEALTH += treasure;
+			sail();
+		}
+		else if (next < 80)
+		{
+			cout << "Thar be a small ship up ahead. Would ye like to attack? (Y/N) " << endl;
+			sail();
+		}
+		else
+		{
+			cout << "A military ship is just under the horizon. Would ye like to attack it? (Y/N) " << endl;
+			sail();
+		}
+	}
+	else if (input == "2")
+	{
+		clear();
+		game_play();
+	}
 }
 
 void build_crew() //2
@@ -96,6 +220,7 @@ void ship_yard() //3
 	if (WEALTH == 0)
 	{
 		cout << "Ye need some wealth to come to the ship yard!" << endl;
+		cout << "Try settin\' sail first." << endl;
 		cout << "(Type C to Continue) : ";
 		cin >> input;
 		while (input != "C")
@@ -113,16 +238,34 @@ void ship_yard() //3
 
 void try_luck() //4
 {
-	clear();
-	game_play();
+	string input = "";
+	if (WEALTH == 0)
+	{
+		cout << "Ye need some wealth to come to the ship yard!" << endl;
+		cout << "Try settin\' sail first." << endl;
+		cout << "(Type C to Continue) : ";
+		cin >> input;
+		while (input != "C")
+		{
+			cin >> input;
+		}
+		clear();
+		game_play();
+	}
+	else
+	{
+		//do stuff
+	}
 }
 
 void status() //5
 {
 	clear();
-	cout << "Status of " << NAME << ": " << endl << endl;
+	cout << "Status of " << NAME << ": " << endl;
+	cout << endl;
 	cout << "Health: " << HEALTH << endl;
 	cout << "Wealth: " << WEALTH << endl;
+	cout << endl;
 	cout << "Ship: " << SHIP << endl;
 	cout << "Ship Health: " << SHIP_HEALTH << endl;
 	cout << "Crew size: " << CREW.size() << endl;
@@ -134,6 +277,15 @@ void status() //5
 			cout << "  " << CREW.at(i) << endl;
 		}
 	}
+	cout << endl;
+	cout << "(Type C to Continue) : ";
+	string input = "";
+	while (input != "C")
+	{
+		cin >> input;
+	}
+	clear();
+	game_play();
 }
 
 void save() //6
@@ -197,6 +349,8 @@ void game_play()
 int main()
 {
 	clear();
+	srand(time(NULL));
+
 	if (init())
 	{
 		tutorial_init();
